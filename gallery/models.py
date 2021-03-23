0fbaa553c1adca06b34376c92281843cb2c8a4d0
estherndichu <itskuijenga@gmail.com>
2021-03-23 04:45:42 +0300

    save and delete location methods

--- gallery/models.py
+++ gallery/models.py
@@ -19,6 +19,13 @@ class Location(models.Model):
     def __str__(self):
         return self.name
 
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+    
+
 class Image(models.Model):
     image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=30)
