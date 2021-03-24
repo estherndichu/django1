8e3612a9efcc83494c988f2ab3f114da2bff4ea6
estherndichu <itskuijenga@gmail.com>
2021-03-25 00:37:21 +0300

    update databases

--- collage/settings.py
+++ collage/settings.py
@@ -77,14 +77,26 @@ WSGI_APPLICATION = 'collage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'beau',
+#         'USER': 'esther',
+#     'PASSWORD':'brielle',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'beau',
-        'USER': 'esther',
-    'PASSWORD':'brielle',
-    }
-}
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': os.environ.get('DB_NAME'),
+           'USER': os.environ.get('DB_USER'),
+           'PASSWORD': os.environ.get('DB_PASSWORD'),
+           'HOST': os.environ.get('DB_HOST'),
+           'PORT': '5432',
+       }
+       
+   }
 
 
 # Password validation
